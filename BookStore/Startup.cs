c773a5c8846301e0b353d98698ea70fe1d03c270
Do Thanh Tan <thanhtan28011998@gmail.com>
2020-06-11 00:48:36 +0700

    Upload bookstorewebsite

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -46,9 +46,11 @@ namespace BookStore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+        //    app.UseHttpsRedirection();
 
             app.UseStaticFiles();
+
+            app.UseCookiePolicy();
 
             app.UseSession();
 
